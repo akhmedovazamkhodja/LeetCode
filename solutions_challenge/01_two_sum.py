997a7d304d1efe41b0535724a60c6adaa1807cbd
akhmedovazamkhodja <azamxoja0091@gmail.com>
2025-12-30 00:48:49 +0500

    1st challenge with solution

---
+++ solutions_challenge/01_two_sum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in lookup:
+                return [lookup[complement], i]
+            lookup[num] = i
